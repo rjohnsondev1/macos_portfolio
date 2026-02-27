08a691e257e9e86f2221a8243ca6c99f0e625e77
rjohnsondev1 <rjohnsondev1@gmail.com>
2026-02-27 09:09:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "#components/Navbar.jsx";
+import Dock from "#components/Dock.jsx";
+
+const App = () => {
+    return (
+       <main>
+           <Navbar />
+           <Dock />
+       </main>
+    )
+}
+export default App
